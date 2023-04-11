7c199cc57036e608a8b3f51dc1723f24585879a0
Leon Zuev <loonatune@gmail.com>
2023-04-11 19:52:20 +0300

    Add sumDig

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println(sumDig(1234));
+  }
+  public static void printBin() {
+
+  }
+  int n = 1234;
+  public static int sumDig(int n) {
+    int result = n % 10;
+    if ( n >= 10) {
+      result += sumDig(n / 10);
+    }
+    return result;
   }
 }
