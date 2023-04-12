8ab9042c638d2bcff96205c397a6e69a068d1cdd
Leon Zuev <loonatune@gmail.com>
2023-04-13 01:17:49 +0300

    Comment main

--- src/fractalNoisePic/Main.java
+++ src/fractalNoisePic/Main.java
@@ -2,6 +2,7 @@ package fractalNoisePic;
 
 import static fractalNoisePic.ShowImageWindow.showImageWindow;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
@@ -12,7 +13,6 @@ public class Main {
   public static void main(String [] args) {
     // Генерация шума
     BufferedImage image = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
-
     FractalNoise fractalNoise = new FractalNoise(512,
         new Random(image.getWidth(),100000),9);
 
@@ -24,8 +24,24 @@ public class Main {
         pixels[pixelIndex++] = 0xff000000 | value << 16 | value << 8 | value;
       }
     }
+/*    int skyColor = 0xFF87CEFA;
+    int skyRed = (skyColor >> 16) & 0xFF;
+    int skyGreen = (skyColor >> 8) & 0xFF;
+    int skyBlue = skyColor & 0xFF;
+    for (int i = 0; i < image.getHeight(); ++i) {
+      for (int j = 0; j < image.getWidth(); ++j) {
+        if (pixelIndex < pixels.length) {
+          float noiseValue = fractalNoise.getValue(j, i);
+          int r = (int) (skyRed * noiseValue);
+          int g = (int) (skyGreen * noiseValue);
+          int b = (int) (skyBlue * noiseValue);
+          pixels[pixelIndex++] = 0xff000000 | (r << 16) | (g << 8) | b;
+        }
+      }
+    }*/
+
     // Создание неба
-    BufferedImage skyImage = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT / 2, BufferedImage.TYPE_INT_RGB);
+   /* BufferedImage skyImage = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT / 2, BufferedImage.TYPE_INT_RGB);
     int skyColor = 0xFF87CEFA;
     for (int i = 0; i < skyImage.getHeight(); ++i) {
       for (int j = 0; j < skyImage.getWidth(); ++j) {
@@ -37,11 +53,16 @@ public class Main {
     terrainImage = TerrainRenderer.renderTerrain(image);
 
     // Объединение неба и горного ландшафта
-    BufferedImage finalImage = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT /2, BufferedImage.TYPE_INT_RGB);
-    finalImage.getGraphics().drawImage(skyImage, 0, 0, null);
-    finalImage.getGraphics().drawImage(terrainImage, 0, IMAGE_HEIGHT / 2, null);
-
+    BufferedImage finalImage = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
+    Graphics g = finalImage.getGraphics();
+    g.drawImage(skyImage, 0, 0, null);
+    g.drawImage(terrainImage, 0, IMAGE_HEIGHT / 2, null);
+*/
+    //небо и ландшафт отбельно:
+/*    showImageWindow(skyImage, IMAGE_WIDTH, IMAGE_HEIGHT / 2);
+    showImageWindow(terrainImage, IMAGE_WIDTH, IMAGE_HEIGHT);
     //output:
-    showImageWindow(finalImage, IMAGE_WIDTH, IMAGE_HEIGHT);
+    showImageWindow(finalImage, IMAGE_WIDTH, IMAGE_HEIGHT);*/
+    showImageWindow(image, IMAGE_WIDTH, IMAGE_HEIGHT);
   }
 }
