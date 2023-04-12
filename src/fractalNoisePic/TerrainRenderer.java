68653db2935a03df05f8cc8b18424c00c1478d63
Leon Zuev <loonatune@gmail.com>
2023-04-12 23:13:13 +0300

    Add TerrainRenderer

---
+++ src/fractalNoisePic/TerrainRenderer.java
@@ -0,0 +1,11 @@
+package fractalNoisePic;
+
+import java.awt.image.BufferedImage;
+
+public class TerrainRenderer {
+  private static final float HEIGHT_SCALE = 10.0f;
+
+  public static BufferedImage renderTerrain(BufferedImage noiseImage) {
+
+  }
+}
